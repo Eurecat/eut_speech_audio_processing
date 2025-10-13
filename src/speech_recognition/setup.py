b28eafb7e857e6bf6b77af4ad790158b4f730e75
joan.omedes <joan.omedes@eurecat.org>
2025-10-13 12:14:12 +0200

    add new launcher

--- src/speech_recognition/setup.py
+++ src/speech_recognition/setup.py
@@ -9,6 +9,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name, ["launch/speech_recognition.launch.py"]),
     ],
     install_requires=["setuptools", "numpy", "torch", "diart"],
     zip_safe=True,
