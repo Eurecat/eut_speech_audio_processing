4e79cc6865af9be15c56e5e5139f4d7ba52e64c3
joan.omedes <joan.omedes@eurecat.org>
2025-10-13 12:13:49 +0200

    new launch file to execute vad, diarization and asr

--- src/audio_stream_manager/launch/audio_stream_manager.launch.py
+++ src/audio_stream_manager/launch/audio_stream_manager.launch.py
@@ -5,22 +5,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
     audio_capturing_node = Node(
         package="audio_stream_manager",
-        executable="audio_capturing_node",
-        name="audio_capturing_node",
+        executable="audio_capturing_select_device_node",
+        name="audio_capturing_select_device_node",
         output="screen",
-        parameters=[{"device_name": "Jabra SPEAK 510 USB: Audio (hw:1,0)"}],
-    )
-
-    audio_device_monitor_node = Node(
-        package="audio_stream_manager",
-        executable="audio_device_monitor_node",
-        name="audio_device_monitor_node",
-        output="screen",
+        # parameters=[{"device_name": "Jabra SPEAK 510 USB: Audio (hw:1,0)"}],
     )
 
     return LaunchDescription(
         [
             audio_capturing_node,
-            audio_device_monitor_node,
         ]
     )
